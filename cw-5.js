70bd2e2bc11ad6e83f330c595ea70004f30cbd32
Aurimas Ž <82368117+Aurimas-stack@users.noreply.github.com>
2022-08-17 14:30:26 +0300

    Update cw-5.js

--- cw-5.js
+++ cw-5.js
@@ -34,3 +34,55 @@ function validISBN10(isbn) {
       11 === 0
   );
 }
+
+// Convert seconds to human readable time
+function humanReadable(seconds) {
+  const time = {
+    hours: (seconds / 60 / 60) >> 0,
+    minutes: 0,
+    seconds: 0,
+  };
+  if (time.hours > 0) {
+    const minutes = seconds - time.hours * 3600;
+    time.minutes = (minutes / 60) >> 0;
+    time.seconds = minutes - ((minutes / 60) >> 0) * 60;
+  }
+  if (time.hours === 0) {
+    time.minutes = (seconds / 60) >> 0;
+    time.seconds = seconds - time.minutes * 60;
+  }
+  if (time.hours === 0 && time.minutes === 0) {
+    time.seconds = seconds;
+  }
+  return `${time.hours > 9 ? time.hours : "0" + time.hours}:${
+    time.minutes > 9 ? time.minutes : "0" + time.minutes
+  }:${time.seconds > 9 ? time.seconds : "0" + time.seconds}`;
+}
+
+// Convert rgb value to hex value
+function rgb(r, g, b){
+  const minMaxValues = (v) => {
+    if(v < 0) {
+      return 0;
+    }
+    if(v > 255) {
+      return 255;
+    }
+    return v;
+  }
+  return [r, g, b].map(v => {
+    const value = minMaxValues(v);
+    const hex = value.toString(16).toUpperCase();
+    if(value === 0) {
+      return hex.repeat(2)
+    }
+    if(value < 10) {
+      return "0" + hex;
+    }
+    if(hex.length === 1) {
+      return "0" + hex;
+    }
+    return hex;
+  }).join("");
+}
+
